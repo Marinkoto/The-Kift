39ec212a4f5fcb54ccc20522f7095e19deda9536
Marinkoto <marinmirev681@gmail.com>
2023-09-23 22:21:16 +0300

    FPS Counter + firerate bug fix

---
+++ Assets/Main Game/Scripts/Loot&UI/FPSCounter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    private TextMeshProUGUI fpsText;
+    private int lastFrameIndex;
+    private float[] frameDeltaTimeArray;
+    private void Awake()
+    {
+        frameDeltaTimeArray = new float[50];
+    }
+    private void Start()
+    {
+        fpsText = GameObject.FindGameObjectWithTag("FPS").GetComponent<TextMeshProUGUI>();
+    }
+    void Update()
+    {
+        frameDeltaTimeArray[lastFrameIndex] = Time.unscaledDeltaTime;
+        lastFrameIndex = (lastFrameIndex + 1) % frameDeltaTimeArray.Length;
+        fpsText.text = "FPS: " + Mathf.RoundToInt(CalculateFPS()).ToString();
+    }
+    private float CalculateFPS()
+    {
+        float total = 0f;
+        foreach (float deltaTime in frameDeltaTimeArray)
+        {
+            total += deltaTime;
+        }
+        return frameDeltaTimeArray.Length / total;
+    }
+}
